a79bc119db485e1cd2c9dd80f974a3a699bf9969
SeafAldeen <95848004+SeafAldeen@users.noreply.github.com>
2022-09-13 20:28:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function openmune(){
 }
 
 let image =document.getElementById('image');
-let images = ['./images/school.jpg','./images/image1.jpg','./images/image2.jpg','./images/image4.jpg']
+let images = ['school.jpg','image1.jpg','image2.jpg','image4.jpg']
 setInterval(function(){
   let random = Math.floor(Math.random() * 4 );
   image.src =images[random];
